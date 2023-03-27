3d7db51bc06ee2f68f4c91aab8b5ed9834a73ca9
unknown <48864283+hibanasama@users.noreply.github.com>
2023-03-27 06:29:33 +0500

    fix: link to license in footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -6,7 +6,7 @@ import { NavigationNode, VersionInfo } from 'app/navigation/navigation.service';
   selector: 'aio-footer',
   template: `
     <p>
-      Documentation code licensed under <a href="https://github.com/ddoodev/ddoodocs/tree/develop/LICENSE">MIT LICENSE</a> and has multiple copyright holders.
+      Documentation code licensed under <a href="https://github.com/ddoodev/ddoocs/tree/develop/LICENSE">MIT LICENSE</a> and has multiple copyright holders.
     </p>
     <p>
       Other code are licensed under <a href="https://github.com/ddoodev/discordoo/tree/develop/LICENSE">MIT License</a>.
