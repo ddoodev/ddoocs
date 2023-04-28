9955972642bd9a6dba193c76faf8b11a73494a08
Hibana <48864283+HibanaSama@users.noreply.github.com>
2023-04-28 19:42:15 +0500

    fix(tools): fix line splitting in index file transformer

--- tools/transforms/ddoocs/transforms/IndexFileTransformer.ts
+++ tools/transforms/ddoocs/transforms/IndexFileTransformer.ts
@@ -1,4 +1,3 @@
-import * as os from 'os';
 import * as ts from 'typescript';
 
 enum ModifyType {
@@ -16,7 +15,7 @@ export class IndexFileTransformer {
   ) {}
 
   transform(sourceFile: ts.SourceFile) {
-    const fileLines = sourceFile.text.split(os.EOL);
+    const fileLines = sourceFile.text.split('\n');
 
     let modifyType: ModifyType | undefined = undefined;
     for (let i = 0; i < fileLines.length; i++) {
@@ -40,10 +39,10 @@ export class IndexFileTransformer {
         continue;
       }
 
-      const splittedLine = line.split(this.prefix);
-      const nextLine = fileLines[i+1];
+      const keyword = line.split(this.prefix)[1].trim().split(' ')[0];
+      const nextLine = fileLines[i + 1];
 
-      switch (splittedLine[1].trim()) {
+      switch (keyword) {
         case this.ignoreWord:
           if (this.exportRegexp.test(nextLine)) {
             modifyType = ModifyType.Ignore;
