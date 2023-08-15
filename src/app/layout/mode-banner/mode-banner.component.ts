2f3d6daca0fbfa22041f2e67778910f11405e71e
Hibana <48864283+HibanaSama@users.noreply.github.com>
2023-08-15 22:33:47 +0500

    feat(ModeBannerComponent): add redirect to current link

--- src/app/layout/mode-banner/mode-banner.component.ts
+++ src/app/layout/mode-banner/mode-banner.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import { VersionInfo } from 'app/navigation/navigation.service';
+import { LocationService } from '../../shared/location.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'aio-mode-banner',
@@ -8,15 +10,32 @@ import { VersionInfo } from 'app/navigation/navigation.service';
           <p>
               This is the <strong>{{ isDev ? 'dev' : 'archived' }} documentation for
               Discordoo{{ versionString }}</strong> Please visit
-              <a href="https://docs.ddoo.dev">docs.ddoo.dev</a>
+              <a href="https://docs.ddoo.dev{{currentPath}}">docs.ddoo.dev</a>
               to see this page for the stable version of Discordoo.
           </p>
       </div>
   `
 })
-export class ModeBannerComponent {
+export class ModeBannerComponent implements OnInit, OnDestroy {
   @Input() mode: string;
   @Input() version: VersionInfo;
+
+  currentPath: string;
+  private currentPathSubscription: Subscription | undefined;
+
+  constructor(private locationService: LocationService) {}
+
+  ngOnInit() {
+    if (this.isArchive || this.isDev) {
+      this.currentPathSubscription = this.locationService.currentPath.subscribe((path) =>
+        this.currentPath = path ?? ''
+      );
+    }
+  }
+
+  ngOnDestroy() {
+    this.currentPathSubscription?.unsubscribe();
+  }
 
   public get isArchive(): boolean {
     return this.mode === 'archive';
